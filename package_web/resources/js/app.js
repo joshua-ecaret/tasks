31b3a0d6188fdda5adcb427ecf58150a995f69a6
Joshua Sajeev <joshua@ecaret.com>
2025-05-31 09:46:00 +0530

    Dir Cleanup

--- package_web/resources/js/app.js
+++
@@ -1,2 +0,0 @@
-import './bootstrap';
-import './packages-form.js';
--- package_web/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\PackageController;
-use Illuminate\Support\Facades\Route;
-
-Route::resource('packages',PackageController::class);
